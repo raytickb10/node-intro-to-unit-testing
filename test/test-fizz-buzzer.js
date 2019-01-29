6b67edd1fe158b4a67e823a71350711023281291
raytickb10 <raytickb10@students.ecu.edu>
2019-01-29 13:43:39 -0500

    Update test-fizz-buzzer.js

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -12,9 +12,9 @@ describe('fizzBuzzer', function() {
     // range of normal inputs, including
     // notable cases like negative answers
     const normalCases = [
-      {a: 5, expected: 5},
-      {a: 2200, expected: 2200},
-      {a: -3, expected: -3}
+      {a: 5, expected: 'buzz'},
+      {a: 3, expected: 'fizz'},
+      {a: 15, expected: 'fizz-buzz'}
     ];
     // for each set of inputs (a, b), `adder` should
     // produce the expected value
